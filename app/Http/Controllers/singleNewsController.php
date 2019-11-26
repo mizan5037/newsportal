bd926f5d6c06021cd0995e0868952b0591374625
mizan5037 <mrahman5037@gmail.com>
2019-11-26 23:28:23 +0600

    details news

--- app/Http/Controllers/singleNewsController.php
+++ app/Http/Controllers/singleNewsController.php
@@ -1,18 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use App\admin\News;
-
+use App\admin\Category;
+use DB;
 
 class singleNewsController extends Controller
 {
-    public function getSingle($category,$slug,$id){
-        $category = ucfirst($category);
+    public function getSingle($category, $slug, $id)
+    {
+        $category   = ucfirst($category);
         $singleNews = News::find($id);
-
-        return view('front.pages.single')->withSinglenews($singleNews);
+        $news       = DB::table('news')
+            ->select('news.*', 'categories.name as category_name')
+            ->leftJoin('categories', 'categories.id', '=', 'news.category_id')
+            ->take(6)->get();
+        $category = Category::all();
+        return view('front.pages.single')
+                                        ->withSinglenews($singleNews)
+                                        ->withCategories($category)
+                                        ->withNews($news);
 
     }
 }
---
+++ resources/views/front/pages/footer.blade.php
@@ -0,0 +1,392 @@
+<div class="video-post-area bg-img bg-overlay" style="background-image: url(front/img/bg-img/bg1.jpg);">
+    <div class="container">
+        <div class="row justify-content-center">
+            <!-- Single Video Post -->
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-video-post">
+                    <img src=" {{ asset('front/img/bg-img/video1.jpg') }}" alt="">
+                    <!-- Video Button -->
+                    <div class="videobtn">
+                        <a href="https://www.youtube.com/watch?v=5BQr-j3BBzU" class="videoPlayer"><i class="fa fa-play"
+                                aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Video Post -->
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-video-post">
+                    <img src=" {{ asset('front/img/bg-img/video2.jpg') }}" alt="">
+                    <!-- Video Button -->
+                    <div class="videobtn">
+                        <a href="https://www.youtube.com/watch?v=5BQr-j3BBzU" class="videoPlayer"><i class="fa fa-play"
+                                aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Video Post -->
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-video-post">
+                    <img src=" {{ asset('front/img/bg-img/video3.jpg') }}" alt="">
+                    <!-- Video Button -->
+                    <div class="videobtn">
+                        <a href="https://www.youtube.com/watch?v=5BQr-j3BBzU" class="videoPlayer"><i class="fa fa-play"
+                                aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ##### Video Post Area End ##### -->
+
+<!-- ##### Editorial Post Area Start ##### -->
+<div class="editors-pick-post-area section-padding-80-50">
+    <div class="container">
+        <div class="row">
+            <!-- Editors Pick -->
+            <div class="col-12 col-md-7 col-lg-9">
+                <div class="section-heading">
+                    <h6>Editor’s Pick</h6>
+                </div>
+
+                <div class="row">
+
+                    <!-- Single Post -->
+                    <div class="col-12 col-lg-4">
+                        <div class="single-blog-post">
+                            <div class="post-thumb">
+                                <a href="#"><img src=" {{ asset('front/img/bg-img/1.jpg') }}" alt=""></a>
+                            </div>
+                            <div class="post-data">
+                                <a href="#" class="post-title">
+                                    <h6>Orci varius natoque penatibus et magnis dis parturient montes.</h6>
+                                </a>
+                                <div class="post-meta">
+                                    <div class="post-date"><a href="#">February 11, 2018</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Single Post -->
+                    <div class="col-12 col-lg-4">
+                        <div class="single-blog-post">
+                            <div class="post-thumb">
+                                <a href="#"><img src=" {{ asset('front/img/bg-img/2.jpg') }}" alt=""></a>
+                            </div>
+                            <div class="post-data">
+                                <a href="#" class="post-title">
+                                    <h6>Orci varius natoque penatibus et magnis dis parturient montes.</h6>
+                                </a>
+                                <div class="post-meta">
+                                    <div class="post-date"><a href="#">February 11, 2018</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Single Post -->
+                    <div class="col-12 col-lg-4">
+                        <div class="single-blog-post">
+                            <div class="post-thumb">
+                                <a href="#"><img src=" {{ asset('front/img/bg-img/3.jpg') }}" alt=""></a>
+                            </div>
+                            <div class="post-data">
+                                <a href="#" class="post-title">
+                                    <h6>Orci varius natoque penatibus et magnis dis parturient montes.</h6>
+                                </a>
+                                <div class="post-meta">
+                                    <div class="post-date"><a href="#">February 11, 2018</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Single Post -->
+                    <div class="col-12 col-lg-4">
+                        <div class="single-blog-post">
+                            <div class="post-thumb">
+                                <a href="#"><img src=" {{ asset('front/img/bg-img/4.jpg') }}" alt=""></a>
+                            </div>
+                            <div class="post-data">
+                                <a href="#" class="post-title">
+                                    <h6>Orci varius natoque penatibus et magnis dis parturient montes.</h6>
+                                </a>
+                                <div class="post-meta">
+                                    <div class="post-date"><a href="#">February 11, 2018</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Single Post -->
+                    <div class="col-12 col-lg-4">
+                        <div class="single-blog-post">
+                            <div class="post-thumb">
+                                <a href="#"><img src=" {{ asset('front/img/bg-img/5.jpg') }}" alt=""></a>
+                            </div>
+                            <div class="post-data">
+                                <a href="#" class="post-title">
+                                    <h6>Orci varius natoque penatibus et magnis dis parturient montes.</h6>
+                                </a>
+                                <div class="post-meta">
+                                    <div class="post-date"><a href="#">February 11, 2018</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Single Post -->
+                    <div class="col-12 col-lg-4">
+                        <div class="single-blog-post">
+                            <div class="post-thumb">
+                                <a href="#"><img src=" {{ asset('front/img/bg-img/6.jpg') }}" alt=""></a>
+                            </div>
+                            <div class="post-data">
+                                <a href="#" class="post-title">
+                                    <h6>Orci varius natoque penatibus et magnis dis parturient montes.</h6>
+                                </a>
+                                <div class="post-meta">
+                                    <div class="post-date"><a href="#">February 11, 2018</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- World News -->
+            <div class="col-12 col-md-5 col-lg-3">
+                <div class="section-heading">
+                    <h6>World News</h6>
+                </div>
+
+                <!-- Single Post -->
+                <div class="single-blog-post style-2">
+                    <div class="post-thumb">
+                        <a href="#"><img src=" {{ asset('front/img/bg-img/7.jpg') }}" alt=""></a>
+                    </div>
+                    <div class="post-data">
+                        <a href="#" class="post-title">
+                            <h6>Orci varius natoque penatibus et magnis</h6>
+                        </a>
+                        <div class="post-meta">
+                            <div class="post-date"><a href="#">February 11, 2018</a></div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Post -->
+                <div class="single-blog-post style-2">
+                    <div class="post-thumb">
+                        <a href="#"><img src=" {{ asset('front/img/bg-img/8.jpg') }}" alt=""></a>
+                    </div>
+                    <div class="post-data">
+                        <a href="#" class="post-title">
+                            <h6>Orci varius natoque penatibus et magnis</h6>
+                        </a>
+                        <div class="post-meta">
+                            <div class="post-date"><a href="#">February 11, 2018</a></div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Post -->
+                <div class="single-blog-post style-2">
+                    <div class="post-thumb">
+                        <a href="#"><img src=" {{ asset('front/') }}img/bg-img/9.jpg" alt=""></a>
+                    </div>
+                    <div class="post-data">
+                        <a href="#" class="post-title">
+                            <h6>Orci varius natoque penatibus et magnis</h6>
+                        </a>
+                        <div class="post-meta">
+                            <div class="post-date"><a href="#">February 11, 2018</a></div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Post -->
+                <div class="single-blog-post style-2">
+                    <div class="post-thumb">
+                        <a href="#"><img src=" {{ asset('front/') }}img/bg-img/10.jpg" alt=""></a>
+                    </div>
+                    <div class="post-data">
+                        <a href="#" class="post-title">
+                            <h6>Orci varius natoque penatibus et magnis</h6>
+                        </a>
+                        <div class="post-meta">
+                            <div class="post-date"><a href="#">February 11, 2018</a></div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Post -->
+                <div class="single-blog-post style-2">
+                    <div class="post-thumb">
+                        <a href="#"><img src=" {{ asset('front/') }}img/bg-img/11.jpg" alt=""></a>
+                    </div>
+                    <div class="post-data">
+                        <a href="#" class="post-title">
+                            <h6>Orci varius natoque penatibus et magnis</h6>
+                        </a>
+                        <div class="post-meta">
+                            <div class="post-date"><a href="#">February 11, 2018</a></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ##### Editorial Post Area End ##### -->
+
+<!-- ##### Footer Add Area Start ##### -->
+<div class="footer-add-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="footer-add">
+                    <a href="#"><img src=" {{ asset('front/img/bg-img/footer-add.gif') }}" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ##### Footer Add Area End ##### -->
+
+<!-- ##### Footer Area Start ##### -->
+<footer class="footer-area">
+
+    <!-- Main Footer Area -->
+    <div class="main-footer-area">
+        <div class="container">
+            <div class="row">
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="footer-widget-area mt-80">
+                        <!-- Footer Logo -->
+                        <div class="footer-logo">
+                            <a href="index.html"><img src=" {{ asset('front/img/core-img/logo.png') }}" alt=""></a>
+                        </div>
+                        <!-- List -->
+                        <ul class="list">
+                            <li><a href="mailto:contact@youremail.com">contact@youremail.com</a></li>
+                            <li><a href="tel:+4352782883884">+43 5278 2883 884</a></li>
+                            <li><a href="http://yoursitename.com">www.yoursitename.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-sm-6 col-lg-2">
+                    <div class="footer-widget-area mt-80">
+                        <!-- Title -->
+                        <h4 class="widget-title">Politics</h4>
+                        <!-- List -->
+                        <ul class="list">
+                            <li><a href="#">Business</a></li>
+                            <li><a href="#">Markets</a></li>
+                            <li><a href="#">Tech</a></li>
+                            <li><a href="#">Luxury</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-sm-4 col-lg-2">
+                    <div class="footer-widget-area mt-80">
+                        <!-- Title -->
+                        <h4 class="widget-title">Featured</h4>
+                        <!-- List -->
+                        <ul class="list">
+                            <li><a href="#">Football</a></li>
+                            <li><a href="#">Golf</a></li>
+                            <li><a href="#">Tennis</a></li>
+                            <li><a href="#">Motorsport</a></li>
+                            <li><a href="#">Horseracing</a></li>
+                            <li><a href="#">Equestrian</a></li>
+                            <li><a href="#">Sailing</a></li>
+                            <li><a href="#">Skiing</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-sm-4 col-lg-2">
+                    <div class="footer-widget-area mt-80">
+                        <!-- Title -->
+                        <h4 class="widget-title">FAQ</h4>
+                        <!-- List -->
+                        <ul class="list">
+                            <li><a href="#">Aviation</a></li>
+                            <li><a href="#">Business</a></li>
+                            <li><a href="#">Traveller</a></li>
+                            <li><a href="#">Destinations</a></li>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Food/Drink</a></li>
+                            <li><a href="#">Hotels</a></li>
+                            <li><a href="#">Partner Hotels</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-sm-4 col-lg-2">
+                    <div class="footer-widget-area mt-80">
+                        <!-- Title -->
+                        <h4 class="widget-title">+More</h4>
+                        <!-- List -->
+                        <ul class="list">
+                            <li><a href="#">Fashion</a></li>
+                            <li><a href="#">Design</a></li>
+                            <li><a href="#">Architecture</a></li>
+                            <li><a href="#">Arts</a></li>
+                            <li><a href="#">Autos</a></li>
+                            <li><a href="#">Luxury</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bottom Footer Area -->
+    <div class="bottom-footer-area">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <!-- Copywrite -->
+                    <p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | This template is made with <i class="fa fa-heart-o"
+                            aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- ##### Footer Area Start ##### -->
+
+<!-- ##### All Javascript Files ##### -->
+<!-- jQuery-2.2.4 js -->
+<script src="{{ asset('front/js/jquery/jquery-2.2.4.min.js') }} "></script>
+<!-- Popper js -->
+<script src="{{ asset('front/js/bootstrap/popper.min.js') }} "></script>
+<!-- Bootstrap js -->
+<script src="{{ asset('front/js/bootstrap/bootstrap.min.js') }} "></script>
+<!-- All Plugins js -->
+<script src="{{ asset('front/js/plugins/plugins.js') }} "></script>
+<!-- Active js -->
+<script src="{{ asset('front/js/active.js') }} "></script>
+</body>
+
+</html>
